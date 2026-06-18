1102e69b52ad700d81ac4a1b021b1f32ab40e4b3
itc-n26003 <n26003@std.it-college.ac.jp>
2026-06-18 07:41:20 +0000

    P104-P115 第7章 演習 終了

---
+++ Chapter07/ex7-05.py
@@ -0,0 +1,8 @@
+tv = ["GOT", "Narcos", "Vice"]
+i = 0
+for show in tv:
+    new = tv[i]
+    new = new.upper()
+    i += 1
+
+print(tv)
---
+++ Chapter07/ex7-06.py
@@ -0,0 +1,7 @@
+l = ['Alice', 'BOb', 'Chalie']
+for i, name in enumerate(l):
+    print(i, name)
+
+# 0 Alice
+# 1 Bob
+# 2 Charlie
